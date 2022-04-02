504aacc0ecc8561b222028c13310567055c53f4f
Anne Tiotuico <annetiotuico@Annes-MacBook-Pro.local>
2022-04-01 19:38:13 -0700

    added another solution

--- objects/testingObjectEquality.js
+++ objects/testingObjectEquality.js
@@ -24,6 +24,15 @@ function objectsEqual(obj1, obj2) {
   return Object.keys(obj1).every(key => obj2[key] && obj1[key] === obj2[key]);
 }
 
+// another solution:
+function objectsEqual(obj1, obj2) {
+  for (let [key, value] of Object.entries(obj1)){
+    if (!obj2.hasOwnProperty(key) || obj2[key] !== value) return false
+  }
+
+  return true
+}
+
 console.log(objectsEqual({a: 'foo'}, {a: 'foo'}));                      // true
 console.log(objectsEqual({a: 'foo', b: 'bar'}, {a: 'foo'}));            // false
 console.log(objectsEqual({}, {}));                                      // true
